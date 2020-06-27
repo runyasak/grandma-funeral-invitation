0ec9d7c7dc2ec06b24c58cde618df35e441f988c
Runyasak Chaengnaimuang <world.jadi@gmail.com>
2020-06-28 00:56:08 +0700

    Add Grandma Image with update flex

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <div id="app" class="bg-primary h-screen">
-    <div class="container mx-auto h-full">
-      <img alt="Vue logo" src="./assets/logo.png">
+    <div class="container mx-auto h-full p-4 flex flex-col justify-center items-center">
+      <div class="max-w-xs rounded overflow-hidden shadow-lg">
+        <img alt="Grandma Image" src="https://firebasestorage.googleapis.com/v0/b/grandma-funeral.appspot.com/o/grandma-mono.jpg?alt=media&token=61ed9474-e14e-4d84-b0ad-a2700953d909">
+      </div>
     </div>
   </div>
 </template>
