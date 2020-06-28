87113ec35bf518243b26d1c0218319cc7a40470f
Runyasak Chaengnaimuang <world.jadi@gmail.com>
2020-06-28 10:23:16 +0700

    Add content and button with style

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,18 @@
 <template>
   <div id="app" class="bg-primary h-screen">
     <div class="container mx-auto h-full p-4 flex flex-col justify-center items-center">
-      <div class="max-w-xs rounded overflow-hidden shadow-lg">
+      <div class="max-w-xs rounded overflow-hidden shadow-lg mb-8">
         <img alt="Grandma Image" src="https://firebasestorage.googleapis.com/v0/b/grandma-funeral.appspot.com/o/grandma-mono.jpg?alt=media&token=61ed9474-e14e-4d84-b0ad-a2700953d909">
       </div>
+
+      <p class="text-2xl text-white mb-4">ขอเชิญร่วมรับฟังสวดพระอภิธรรมศพ</p>
+      <p class="text-2xl text-white mb-4">คุณแม่ สมปอง แจ้งในเมือง</p>
+
+      <p class="text-xl text-white mb-8">ณ วัดสรรเพชญ อำเภอสามพราน จังหวัดนครปฐม</p>
+
+      <button class="border border-white text-white font-bold py-2 px-4 rounded-full">
+        สร้างบัตรเชิญร่วมงานผ่าน LINE
+      </button>
     </div>
   </div>
 </template>
